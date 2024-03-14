33df62f5801aa80110e179cf7800ce92e3b5c306
Jolanta268549 <268549@student.pwr.edu.pl>
2024-03-14 12:36:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# weather_project
+ Weather station - ardiuno nano esp32, dht11 sensor, OLED 0,96 SSD 1306, blynk app
---
+++ weather_project.ino
@@ -0,0 +1,65 @@
+#define BLYNK_TEMPLATE_ID "TMPL4-K8OKxWx"
+#define BLYNK_TEMPLATE_NAME "działaj prosze"
+#define BLYNK_AUTH_TOKEN "cVPHnSQ0DfSgl9Q5Euf8Bpk2TbwCro_Y"
+
+//Tech Trends Shameer
+
+#define BLYNK_PRINT Serial
+#include <WiFi.h>
+//#include <ESP8266WiFi.h> 
+#include <BlynkSimpleEsp32.h>
+
+#include <DHT.h>
+
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "AndroidAPE";  // type your wifi name
+char pass[] = "";  // type your wifi password
+
+BlynkTimer timer;
+
+
+#define DHTPIN 4 //Connect Out pin to D2 in NODE MCU
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+    Blynk.virtualWrite(V0, t);
+    Blynk.virtualWrite(V1, h);
+    Serial.print("Temperature : ");
+    Serial.print(t);
+    Serial.print("    Humidity : ");
+    Serial.println(h);
+}
+void setup()
+{   
+  
+  Serial.begin(115200);
+  Serial.begin(9600);
+
+  
+
+  Blynk.begin(auth, ssid, pass);
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  dht.begin();
+  timer.setInterval(100L, sendSensor);
+ 
+  }
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+ }
